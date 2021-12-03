33d822ef6e4feb109f097277e3562e9979737815
Nitin <nsinghboss02@gmail.com>
2021-12-03 20:26:41 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,25 @@
+package com.codechef;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	* * * * * *
+	  * * * *
+	   * * *
+	    * *
+	     *
+	             */
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            System.out.print(" ");
+        }
+        for(int k=0;k<=(n-i+1);k++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
